b1224a74d7ca12f52e49b7c0c581ef25bc85642b
Maykel <jotasenator@gmail.com>
2023-06-11 16:01:15 -0400

    Routes 2 approach

--- hello/views.py
+++ hello/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('hello world, Django!!')
+
+def greet(request,name):
+    return HttpResponse(f'Hello, {name}!')
