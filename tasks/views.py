69f9d9afb74bc301c51a43c69ddfb996d7bca126
Maykel <jotasenator@gmail.com>
2023-06-11 18:36:24 -0400

    Create a built in form

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django import forms
 
 tasks=['foo','bar','baz']
+
+class NewTaskForm(forms.Form):
+    task=forms.CharField(label='New Task')
+    priority=forms.IntegerField(label='Priority',min_value=1,max_value=10)
 
 # Create your views here.
 def index(request):
@@ -8,4 +13,6 @@ def index(request):
         'tasks':tasks
     })
 def add(request):
-    return render(request,'tasks/add.html')
+    return render(request,'tasks/add.html',{
+        'form':NewTaskForm()
+    })
