f57d47f6a5efcfaaa19df26ef7703af02a9760b5
Maykel <jotasenator@gmail.com>
2023-06-11 16:37:12 -0400

    Add another html

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('bye', views.bye, name='bye'),
     path('<str:name>',views.greet,name='greet')
 ]
--- hello/views.py
+++ hello/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse('hello world, Django!!')
+
 def index(request):
-    return HttpResponse('hello world, Django!!')
+    return render(request,'hello/index.html')
+
+def bye(request):
+    return render(request,'bye/index.html')
 
 def greet(request,name):
     return HttpResponse(f'Hello, {name.capitalize()}!')
