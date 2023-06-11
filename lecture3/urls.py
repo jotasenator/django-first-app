e0ebfa0c2350306e9ff68347ac29179fdfba45a5
Maykel <jotasenator@gmail.com>
2023-06-11 18:06:54 -0400

    Create layout page for extend

--- lecture3/urls.py
+++ lecture3/urls.py
@@ -20,5 +20,6 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('newyear/',include('newyear.urls')),
-    path('hello/',include('hello.urls'))
+    path('hello/',include('hello.urls')),
+    path('tasks/',include('tasks.urls')),
 ]
